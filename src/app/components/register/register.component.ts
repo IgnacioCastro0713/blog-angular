dedbe2b507fb2181fc16e6ef079af90d2e68429b
ignacio <ignaciomenchaca666@gmail.com>
2019-08-10 01:11:47 -0500

    component login and register singleton in category model (pending)

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  public title: string;
+
+  constructor() {
+    this.title = 'register'
+  }
+
+  ngOnInit() {
+  }
+
+}
